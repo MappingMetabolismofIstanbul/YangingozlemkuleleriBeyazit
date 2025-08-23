c585caf4839bac2b6bc8f05b16ba8b97238d32fc
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-08-23 14:40:18 +0300

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ var config = {
     title: 'Yangın İzleme Kuleleri - Beyazıt ve Galata Kulesi / Fire Watch Towers – Beyazıt and Galata Tower',
     subtitle: '',
     byline: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
-    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    footer: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
     chapters: [
         {
             id: '1',
